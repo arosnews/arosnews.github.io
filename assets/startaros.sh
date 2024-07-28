c7df81a90b96d09e1958d10b9e001d8f8426a4cf
arosnews <153375145+arosnews@users.noreply.github.com>
2024-07-28 19:21:54 +0200

    Add files via upload

---
+++ assets/startaros.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+NETON=`ifconfig | grep aros0`
+
+if [ "$NETON" = "" ]; then
+    echo "Installing bridge network... (sudo access required)"
+    sudo tunctl -b -u "$USER" -t aros0
+    sudo ifconfig aros0 192.168.166.1
+    sudo iptables -t nat -A POSTROUTING -o eno1 -s 192.168.166.0/2 -j MASQUERADE
+    sudo iptables -t nat -A POSTROUTING -o wlp2s0 -s 192.168.166.0/2 -j MASQUERADE
+    sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+fi
+
+cd /home/rfz/AROS/boot/linux/
+./AROSBootstrap -m 1024
